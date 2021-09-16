01d50caa00e3317ca8504693e21c06c09b584cf9
Alex Ferro <aferr070@fiu.edu>
2021-09-15 20:40:46 -0400

    added basic geosearch

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,12 +1,43 @@
-import React, {Component} from "react";
-import { MapContainer, TileLayer, GeoJSON, LayersControl } from "react-leaflet";
-import countyData from './Data/countyBuyouts.json'
-import regionData from './Data/regionalBuyouts.json'
-import muniData from './Data/munigeojson.json'
-import tribalData from './Data/tribalgeojson.json'
+import React, { Component, useEffect } from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  GeoJSON,
+  LayersControl,
+  useMap,
+} from 'react-leaflet';
+import countyData from './Data/countyBuyouts.json';
+import regionData from './Data/regionalBuyouts.json';
+import muniData from './Data/munigeojson.json';
+import tribalData from './Data/tribalgeojson.json';
 import L from 'leaflet';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-geosearch/dist/geosearch.css';
+import icon from "./constants";
 
-const position = [37.1, -95.7]
+const position = [37.1, -95.7];
+
+// leaflet geosearch search bar
+function LeafletgeoSearch() {
+  const map = useMap();
+  useEffect(() => {
+    const provider = new OpenStreetMapProvider();
+
+    const searchControl = new GeoSearchControl({
+      provider,
+      marker: {
+       icon,
+      },
+    });
+
+    map.addControl(searchControl);
+
+    return () => map.removeControl(searchControl);
+  }, []);
+
+  return null;
+}
 
 //const muniDataArray = Array.from(muniData);
 class MyMap extends Component {
@@ -17,211 +48,233 @@ class MyMap extends Component {
   }
   countyStyle = {
     fillOpacity: 1,
-    color: "black",
-    weight: .5,
-  
+    color: 'black',
+    weight: 0.5,
   };
 
   regionStyle = {
     fillOpacity: 1,
-    color: "black",
-    weight: .5,
-  
+    color: 'black',
+    weight: 0.5,
   };
 
   muniStyle = {
     fillOpacity: 1,
-    color: "black",
-    weight: .5,
-  
+    color: 'black',
+    weight: 0.5,
   };
 
   tribalStyle = {
     fillOpacity: 1,
-    color: "black",
-    weight: .5,
-  
-  };
-  
-  printMesssageToConsole = (event) => {
-    console.log("Clicked");
+    color: 'black',
+    weight: 0.5,
   };
 
+  printMesssageToConsole = (event) => {
+    console.log('Clicked');
+  };
 
   onEachCounty = (county, layer) => {
     function getColor(d) {
-      return d > 50  ? '#08306b' :
-             d > 15  ? '#2171b5' :
-             d > 5   ? '#6baed6' :
-             d > 1   ? '#deebf7' :
-                        '#f7fbff';
-  }
+      return d > 50
+        ? '#08306b'
+        : d > 15
+        ? '#2171b5'
+        : d > 5
+        ? '#6baed6'
+        : d > 1
+        ? '#deebf7'
+        : '#f7fbff';
+    }
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 0,
       maximumFractionDigits: 0,
     });
-    const buyoutCounty = county.properties.county
-    const buyoutState = county.properties.state
-    const buyoutGrantcount = county.properties.grantcount
-    const buyoutDollaramount = formatter.format(county.properties.dollaramount)
+    const buyoutCounty = county.properties.county;
+    const buyoutState = county.properties.state;
+    const buyoutGrantcount = county.properties.grantcount;
+    const buyoutDollaramount = formatter.format(county.properties.dollaramount);
 
-    const countyName = "<b>County: </b>" + buyoutCounty 
-    + "<br><b>State: </b>" + buyoutState
-    + "<br><b>Grant Count: </b>" + buyoutGrantcount
-    + "<br><b>Dollar Amount: </b>" + buyoutDollaramount;
+    const countyName =
+      '<b>County: </b>' +
+      buyoutCounty +
+      '<br><b>State: </b>' +
+      buyoutState +
+      '<br><b>Grant Count: </b>' +
+      buyoutGrantcount +
+      '<br><b>Dollar Amount: </b>' +
+      buyoutDollaramount;
     console.log(countyName);
     layer.bindTooltip(countyName);
-    
-    layer.options.fillColor = getColor(county.properties.grantcount);
 
+    layer.options.fillColor = getColor(county.properties.grantcount);
   };
 
   onEachRegion = (region, layer) => {
     function getColor(d) {
-      return d > 1  ? '#7f2704' :
-                        '#fff5eb';
-  }
+      return d > 1 ? '#7f2704' : '#fff5eb';
+    }
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 0,
       maximumFractionDigits: 0,
     });
-    const buyoutCounty = region.properties.county
-    const buyoutSubgrantee = region.properties.subgrantee_clean
-    const buyoutState = region.properties.state
-    const buyoutGrantcount = region.properties.grantcount
-    const buyoutDollaramount = formatter.format(region.properties.dollaramount)
+    const buyoutCounty = region.properties.county;
+    const buyoutSubgrantee = region.properties.subgrantee_clean;
+    const buyoutState = region.properties.state;
+    const buyoutGrantcount = region.properties.grantcount;
+    const buyoutDollaramount = formatter.format(region.properties.dollaramount);
 
-    const countyName = "<b>County: </b>" + buyoutCounty 
-    + "<br><b>Entity: </b>" + buyoutSubgrantee
-    + "<br><b>State: </b>" + buyoutState
-    + "<br><b>Grant Count: </b>" + buyoutGrantcount
-    + "<br><b>Dollar Amount: </b>" + buyoutDollaramount;
+    const countyName =
+      '<b>County: </b>' +
+      buyoutCounty +
+      '<br><b>Entity: </b>' +
+      buyoutSubgrantee +
+      '<br><b>State: </b>' +
+      buyoutState +
+      '<br><b>Grant Count: </b>' +
+      buyoutGrantcount +
+      '<br><b>Dollar Amount: </b>' +
+      buyoutDollaramount;
     console.log(countyName);
     layer.bindTooltip(countyName);
-    
-    layer.options.fillColor = getColor(region.properties.grantcount);
 
+    layer.options.fillColor = getColor(region.properties.grantcount);
   };
 
   onEachMuni = (muni, layer) => {
     function getColor(d) {
-      return d > 20  ? '#67000d' :
-             d > 10  ? '#ef3b2c' :
-             d > 2   ? '#fcbba1' :
-             d > 1   ? '#fcbba1' :
-                        '#fff5f0';
-  }
+      return d > 20
+        ? '#67000d'
+        : d > 10
+        ? '#ef3b2c'
+        : d > 2
+        ? '#fcbba1'
+        : d > 1
+        ? '#fcbba1'
+        : '#fff5f0';
+    }
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 0,
       maximumFractionDigits: 0,
     });
-    const buyoutCounty = muni.properties.county
-    const buyoutSubgrantee = muni.properties.subgrantee_clean
-    const buyoutState = muni.properties.state
-    const buyoutGrantcount = muni.properties.grantcount
-    const buyoutDollaramount = formatter.format(muni.properties.dollaramount)
+    const buyoutCounty = muni.properties.county;
+    const buyoutSubgrantee = muni.properties.subgrantee_clean;
+    const buyoutState = muni.properties.state;
+    const buyoutGrantcount = muni.properties.grantcount;
+    const buyoutDollaramount = formatter.format(muni.properties.dollaramount);
 
-    const muniName = "<b>County: </b>" + buyoutCounty 
-    + "<br><b>Municipality: </b>" + buyoutSubgrantee
-    + "<br><b>State: </b>" + buyoutState
-    + "<br><b>Grant Count: </b>" + buyoutGrantcount
-    + "<br><b>Dollar Amount: </b>" + buyoutDollaramount;
+    const muniName =
+      '<b>County: </b>' +
+      buyoutCounty +
+      '<br><b>Municipality: </b>' +
+      buyoutSubgrantee +
+      '<br><b>State: </b>' +
+      buyoutState +
+      '<br><b>Grant Count: </b>' +
+      buyoutGrantcount +
+      '<br><b>Dollar Amount: </b>' +
+      buyoutDollaramount;
     console.log(muniName);
     layer.bindTooltip(muniName);
-    
+
     layer.options.fillColor = getColor(muni.properties.grantcount);
-    
   };
 
   onEachTribal = (tribal, layer) => {
     function getColor(d) {
-      return d > 1  ? '#00441b' :
-                        '#f7fcf5';
-  }
+      return d > 1 ? '#00441b' : '#f7fcf5';
+    }
     const formatter = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 0,
       maximumFractionDigits: 0,
     });
-    const buyoutCounty = tribal.properties.county
-    const buyoutSubgrantee = tribal.properties.subgrantee_clean
-    const buyoutState = tribal.properties.state
-    const buyoutGrantcount = tribal.properties.grantcount
-    const buyoutDollaramount = formatter.format(tribal.properties.dollaramount)
+    const buyoutCounty = tribal.properties.county;
+    const buyoutSubgrantee = tribal.properties.subgrantee_clean;
+    const buyoutState = tribal.properties.state;
+    const buyoutGrantcount = tribal.properties.grantcount;
+    const buyoutDollaramount = formatter.format(tribal.properties.dollaramount);
 
-    const tribalName = "<b>County: </b>" + buyoutCounty 
-    + "<br><b>Tribal Nation: </b>" + buyoutSubgrantee
-    + "<br><b>State: </b>" + buyoutState
-    + "<br><b>Grant Count: </b>" + buyoutGrantcount
-    + "<br><b>Dollar Amount: </b>" + buyoutDollaramount;
+    const tribalName =
+      '<b>County: </b>' +
+      buyoutCounty +
+      '<br><b>Tribal Nation: </b>' +
+      buyoutSubgrantee +
+      '<br><b>State: </b>' +
+      buyoutState +
+      '<br><b>Grant Count: </b>' +
+      buyoutGrantcount +
+      '<br><b>Dollar Amount: </b>' +
+      buyoutDollaramount;
     console.log(tribalName);
     layer.bindTooltip(tribalName);
-    
+
     layer.options.fillColor = getColor(tribal.properties.grantcount);
-    
   };
 
   pointToLayer(feature, latlng) {
     return L.circleMarker(latlng, {
       fillOpacity: 1,
       radius: 3,
-      stroke: true
-    })
+      stroke: true,
+    });
   }
 
   render() {
     return (
       <div>
-      <h1 style={{ textAlign: "center" }}>FEMA Buyouts by County</h1>
-      <MapContainer 
-        center={position} 
-        zoom={4} 
-        style={{ height: 500, width: "100%" }}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-              url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
-            />
-        <LayersControl position="topright">
-          <LayersControl.Overlay checked name="Counties">
-                <GeoJSON
-                  style={this.countyStyle}
-                  data={countyData.features}
-                  onEachFeature={this.onEachCounty}
-                />
-          </LayersControl.Overlay>
-          <LayersControl.Overlay checked name="Municipalities">
-                <GeoJSON
-                  style={this.muniStyle}
-                  data={muniData.features}
-                  onEachFeature={this.onEachMuni}
-                  pointToLayer={this.pointToLayer.bind(this)}
-                /> 
-          </LayersControl.Overlay>
-          <LayersControl.Overlay checked name="Regional Entities">
-                <GeoJSON
-                  style={this.regionStyle}
-                  data={regionData.features}
-                  onEachFeature={this.onEachRegion}
-                />
-          </LayersControl.Overlay>
-          <LayersControl.Overlay checked name="Tribal Nations">
-                <GeoJSON
-                  style={this.tribalStyle}
-                  data={tribalData.features}
-                  onEachFeature={this.onEachTribal}
-                  pointToLayer={this.pointToLayer.bind(this)}
-                /> 
-          </LayersControl.Overlay>
-        </LayersControl>
-      </MapContainer>
-    </div>
+        <h1 style={{ textAlign: 'center' }}>FEMA Buyouts by County</h1>
+        <MapContainer
+          center={position}
+          zoom={4}
+          style={{ height: 500, width: '100%' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url='https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png'
+          />
+          <LayersControl position='topright'>
+            <LayersControl.Overlay checked name='Counties'>
+              <GeoJSON
+                style={this.countyStyle}
+                data={countyData.features}
+                onEachFeature={this.onEachCounty}
+              />
+            </LayersControl.Overlay>
+            <LayersControl.Overlay checked name='Municipalities'>
+              <GeoJSON
+                style={this.muniStyle}
+                data={muniData.features}
+                onEachFeature={this.onEachMuni}
+                pointToLayer={this.pointToLayer.bind(this)}
+              />
+            </LayersControl.Overlay>
+            <LayersControl.Overlay checked name='Regional Entities'>
+              <GeoJSON
+                style={this.regionStyle}
+                data={regionData.features}
+                onEachFeature={this.onEachRegion}
+              />
+            </LayersControl.Overlay>
+            <LayersControl.Overlay checked name='Tribal Nations'>
+              <GeoJSON
+                style={this.tribalStyle}
+                data={tribalData.features}
+                onEachFeature={this.onEachTribal}
+                pointToLayer={this.pointToLayer.bind(this)}
+              />
+            </LayersControl.Overlay>
+          </LayersControl>
+          <LeafletgeoSearch />
+        </MapContainer>
+      </div>
     );
   }
 }
---
+++ src/Components/Search.js
@@ -0,0 +1,20 @@
+function LeafletgeoSearch() {
+    const map = useMap(); //here use useMap hook
+  
+    useEffect(() => {
+      const provider = new OpenStreetMapProvider();
+  
+      const searchControl = new GeoSearchControl({
+        provider,
+        marker: {
+          icon
+        }
+      });
+  
+      map.addControl(searchControl);
+  
+      return () => map.removeControl(searchControl)
+    }, []);
+  
+    return null;
+  }
