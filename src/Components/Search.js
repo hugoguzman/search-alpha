4751e46cba7dc94be3d1647d74354d342c689675
Alex Ferro <aferr070@fiu.edu>
2021-09-18 12:04:19 -0400

    removed redundant code

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,20 +1,20 @@
-function LeafletgeoSearch() {
-    const map = useMap(); //here use useMap hook
+// function LeafletgeoSearch() {
+//     const map = useMap(); //here use useMap hook
   
-    useEffect(() => {
-      const provider = new OpenStreetMapProvider();
+//     useEffect(() => {
+//       const provider = new OpenStreetMapProvider();
   
-      const searchControl = new GeoSearchControl({
-        provider,
-        marker: {
-          icon
-        }
-      });
+//       const searchControl = new GeoSearchControl({
+//         provider,
+//         marker: {
+//           icon
+//         }
+//       });
   
-      map.addControl(searchControl);
+//       map.addControl(searchControl);
   
-      return () => map.removeControl(searchControl)
-    }, []);
+//       return () => map.removeControl(searchControl)
+//     }, []);
   
-    return null;
-  }
+//     return null;
+//   }
