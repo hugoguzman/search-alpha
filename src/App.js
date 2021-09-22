a5948b972f64ea029ec3994e3d25a81a07a7615b
Alex Ferro <aferr070@fiu.edu>
2021-09-22 17:14:15 -0400

    added AppBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import MyMap from './Components/MyMap'
+import MyMap from './Components/MyMap' 
+import DenseAppBar from './Components/AppBar';
 
 function App() {
   return (
     <div className="App">
+      <DenseAppBar />
       <MyMap />
     </div>
   );
---
+++ src/Components/AppBar.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+// import ReactDOM from 'react-dom';
+// import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import { Menu as MenuIcon } from "@material-ui/icons";
+
+export default function DenseAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            FEMA Buyouts U.S. Map
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+
